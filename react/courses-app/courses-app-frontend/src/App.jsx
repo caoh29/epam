b89df8fb4e60832ed15b48041f4ae9d32d143939
Camilo Ordoñez <cronox20@gmail.com>
2023-04-25 14:48:14 -0500

    adding useParams but looks like its not important

--- react/courses-app/courses-app-frontend/src/App.jsx
+++ react/courses-app/courses-app-frontend/src/App.jsx
@@ -3,7 +3,7 @@ import Courses from './components/Courses/Courses';
 import Header from './components/Header/Header';
 import CreateCourse from './components/CreateCourse/CreateCourse';
 import { useState, Fragment } from 'react';
-import { BrowserRouter, Routes,  Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes,  Route, Navigate, useParams } from 'react-router-dom';
 import { Registration } from '../../courses-app-backend-master/src/components/Registration/Registration';
 import { Login } from '../../courses-app-backend-master/src/components/Login/Login';
 import { CourseInfo } from '../../courses-app-backend-master/src/components/CourseInfo/CourseInfo';
@@ -23,6 +23,7 @@ function App() {
     setShowCreateCourse(false);
   };
 
+  // const { courseId } = useParams();
 
   return (
     <BrowserRouter>
