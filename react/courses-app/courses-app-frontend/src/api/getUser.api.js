081784af1b80623e8e58c6a23fbbb949cac76305
Camilo Ordoñez <cronox20@gmail.com>
2023-04-24 21:24:16 -0500

    finishing loggin !!!!

--- react/courses-app/courses-app-frontend/src/api/getUser.api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const apiUrl = 'http://localhost:4000/users/me';
-const headers = {
-    'accept': '*/*',
-};
-export const getUser = () => {
-    return axios.get(apiUrl, { headers });
-};
